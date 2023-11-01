3b4a2e340d1c8032db6143d267cba394bd229cc4
gickcy <gickcyannabiju@gmail.com>
2023-11-01 12:31:07 +0530

    first commit

---
+++ apptodo/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+app_name = 'apptodo'
+urlpatterns = [
+
+    path('',views.add,name='add'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+    path('cbvdetail/<int:pk>/',views.TaskDetailtview.as_view(),name='cbvdetail'),
+    path('cbvupdate/<int:pk>/',views.TaskUpdateview.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.TaskDeletetview.as_view(),name='cbvdelete')
+
+
+
+]
